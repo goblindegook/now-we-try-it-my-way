0faeb2c2edc0a35039b482715d927de8fd48cefb
Luis Rodrigues <lrodrigues@newstore.com>
2026-05-23 14:27:32 +0200

    feat: fennel orange salad

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,15 @@ export default defineConfig({
   vite: {
     plugins: [{
       name: 'cook-hmr',
-      handleHotUpdate({ file, server }) {
+      configureServer(server) {
+        const restartOnCookFileChange = (file) => {
+          if (file.endsWith('.cook')) server.restart();
+        };
+
+        server.watcher.on('add', restartOnCookFileChange);
+        server.watcher.on('unlink', restartOnCookFileChange);
+      },
+      handleHotUpdate({ file, modules, server, timestamp }) {
         if (file.endsWith('.cook')) {
           server.ws.send({ type: 'full-reload' });
           return [];
